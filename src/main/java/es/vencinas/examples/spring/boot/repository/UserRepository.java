fa76bf98fbdb0064b90df8fe9165ce7cd79b5f89
Valentin Encinas <vencinas@valentinencinas.es>
2016-12-15 00:41:24 +0100

    Added junit independent of the database. Extended the search functions implemented in the User repository.

--- src/main/java/es/vencinas/examples/spring/boot/repository/UserRepository.java
+++ src/main/java/es/vencinas/examples/spring/boot/repository/UserRepository.java
@@ -5,14 +5,19 @@ import java.util.Collection;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.data.rest.core.annotation.RestResource;
 
 import es.vencinas.examples.spring.boot.dao.UserDAO;
 
+@RepositoryRestResource
 public interface UserRepository extends JpaRepository<UserDAO, Long> {
 
     // select * from UserDAO where firstname = :name
+    @RestResource(path = "by-firstname")
     public Collection<UserDAO> findAllByFirstname(String name);
 
+    @RestResource(path = "by-lower-firstname")
     @Query("from UserDAO where lower(firstname) like lower(?1)")
     UserDAO findByNameWithQuery(@Param("firstname") String name);
 }
